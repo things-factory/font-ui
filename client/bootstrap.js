8f53d62e5a795536a8932cf2e96b304b8e1428a5
Hearty Oh <heartyoh@hatiolab.com>
2019-10-08 11:51:01 +0800

    v0.0.2

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -1,9 +1,1 @@
-import { store } from '@things-factory/shell'
-import { fetchFontList } from '@things-factory/font-base'
-import { auth } from '@things-factory/auth-base'
-
-export default function bootstrap() {
-  auth.on('signin', () => {
-    store.dispatch(fetchFontList({}))
-  })
-}
+export default function bootstrap() {}
